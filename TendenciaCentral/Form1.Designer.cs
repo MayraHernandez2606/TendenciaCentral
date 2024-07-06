a8bf35216e2dcfb47cc3c397611332a5b8b29840
Saraí Hernández <msha17011984@gmail.com>
2024-07-05 21:08:06 -0600

    Agregar archivos de proyecto.

---
+++ TendenciaCentral/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace TendenciaCentral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.TablaDatos = new System.Windows.Forms.DataGridView();
+            this.datos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumDatos = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Sienna;
+            this.label1.Location = new System.Drawing.Point(112, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(580, 74);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MEDIA ARITMETICA";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TendenciaCentral.Properties.Resources._2709774;
+            this.pictureBox1.Location = new System.Drawing.Point(188, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDatos.Location = new System.Drawing.Point(9, 486);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(279, 37);
+            this.lblDatos.TabIndex = 2;
+            this.lblDatos.Text = "Introduzca sus datos:";
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(307, 498);
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(293, 22);
+            this.txtDatos.TabIndex = 3;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 606);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(209, 37);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Su resultado es:";
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(224, 648);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(175, 22);
+            this.txtMedia.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalcular.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCalcular.Location = new System.Drawing.Point(623, 559);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(166, 63);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // TablaDatos
+            // 
+            this.TablaDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TablaDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.datos});
+            this.TablaDatos.Location = new System.Drawing.Point(804, 86);
+            this.TablaDatos.Name = "TablaDatos";
+            this.TablaDatos.RowHeadersWidth = 51;
+            this.TablaDatos.RowTemplate.Height = 24;
+            this.TablaDatos.Size = new System.Drawing.Size(204, 611);
+            this.TablaDatos.TabIndex = 10;
+            // 
+            // datos
+            // 
+            this.datos.HeaderText = "DATOS";
+            this.datos.MinimumWidth = 6;
+            this.datos.Name = "datos";
+            this.datos.Width = 125;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 526);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(405, 37);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Introduzca su numero de datos:";
+            // 
+            // txtNumDatos
+            // 
+            this.txtNumDatos.Location = new System.Drawing.Point(432, 538);
+            this.txtNumDatos.Name = "txtNumDatos";
+            this.txtNumDatos.Size = new System.Drawing.Size(168, 22);
+            this.txtNumDatos.TabIndex = 12;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAgregar.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAgregar.Location = new System.Drawing.Point(623, 487);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(166, 66);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSalir.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSalir.Location = new System.Drawing.Point(623, 628);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(166, 66);
+            this.btnSalir.TabIndex = 15;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 706);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNumDatos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TablaDatos);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.TextBox txtDatos;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataGridView TablaDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumDatos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ TendenciaCentral/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TendenciaCentral
+{
+    public partial class Form1 : Form
+    {
+        int n = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double sum = 0, med = 0, dato2 = 0;
+            
+            // Iterar a través de las filas del DataGridView y sumar los valores de la columna "Datos"
+            foreach (DataGridViewRow row in TablaDatos.Rows)
+            {
+                if (row.Cells["datos"].Value != null)
+                {
+                    sum += Convert.ToDouble(row.Cells["datos"].Value);
+                }
+            }
+
+            dato2 = Convert.ToDouble(txtNumDatos.Text);
+
+            // Aplicacion de la media aritmetica de los datos
+            med = (sum) / dato2 ;
+            txtMedia.Text = Convert.ToString(med);
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Agregar un nuevo reglon
+            int n = TablaDatos.Rows.Add();
+
+            //Colocar la informacion en los textbox
+            TablaDatos.Rows[n].Cells[0].Value = txtDatos.Text;
+
+            //Limpiar los text box
+            txtDatos.Text = "";
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
